d7e8a7409f561591bcb997a8326367cd1e8c0eaa
kavish chittora <52811068+kavish729@users.noreply.github.com>
2022-05-16 17:44:09 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import List1 from './component/List1';
+import List2 from './component/List2';
+
+function App() {
+  return (
+    <div>
+     <List1/>
+     <List2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/List1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const List1 = () => {
+  return (
+    <div>
+  <h1>Mobile Operating System</h1>
+  <ul>
+      <li>Andriod</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windos Phone</li>
+  </ul>
+    </div>
+  )
+}
+
+export default List1
